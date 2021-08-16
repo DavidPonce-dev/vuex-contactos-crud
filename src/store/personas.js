71744d7bc621e5064580086bbf13513363e12560
DavidPonce-dev <60764076+DavidPonce-dev@users.noreply.github.com>
2021-08-16 14:22:19 -0400

    separando logica del componente Error en un Store module

--- src/store/personas.js
+++ src/store/personas.js
@@ -7,13 +7,16 @@ export default {
   mutations: {
     addPersona({ personas }, persona) {
       personas.push(persona)
+      //throw new Error('Error agregando persona')
     },
     setPersona(state, persona) {
       state.personas[state.editId] = persona
       state.editId = null
+      //throw new Error('Error editando persona')
     },
     delPersona(state, persona) {
       state.personas = state.personas.filter( p => p.id != persona.id )
+      //throw new Error('Error borrando persona')
     },
     setEditId(state, id) {
       state.editId = id
