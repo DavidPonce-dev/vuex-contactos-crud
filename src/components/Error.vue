8ab2077508df8e487926569dd01a8be940f2507d
DavidPonce-dev <60764076+DavidPonce-dev@users.noreply.github.com>
2021-08-16 00:26:26 -0400

    creando componente externo

---
+++ src/components/Error.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="error alert alert-danger">
+    {{ error }}
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    error: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style scoped>
+.error {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+}
+</style>
